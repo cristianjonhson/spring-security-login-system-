1a5ebb2229ba29297d571f31d134023906535453
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-01-28 23:50:40 -0300

    initial commit

---
+++ AuthenticatedBackend/src/test/java/cl/empresa/AuthenticatedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.empresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
