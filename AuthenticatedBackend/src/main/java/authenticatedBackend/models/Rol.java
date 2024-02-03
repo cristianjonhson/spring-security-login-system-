7cccbe7ddd15e8ef43453b683118b79cf8b85c27
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-02-02 23:13:13 -0300

    explicacion modelo Rol

--- AuthenticatedBackend/src/main/java/authenticatedBackend/models/Rol.java
+++ AuthenticatedBackend/src/main/java/authenticatedBackend/models/Rol.java
@@ -9,33 +9,39 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+// La clase representa un rol de seguridad y implementa la interfaz GrantedAuthority de Spring Security.
 @Entity
 @Table(name="roles")
 public class Rol implements GrantedAuthority {
 	
+	// Identificador único del rol en la base de datos.
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id_rol")
 	private Integer roleId;
 	
+	// Nombre del rol (autoridad) que se utilizará en la lógica de seguridad de Spring.
 	private String authority;
 	
+	// Constructor predeterminado requerido por JPA.
 	public Rol() {
 		super();
 	}
 	
+	// Constructor que toma el nombre del rol como parámetro.
 	public Rol(String authority) {
 		this.authority = authority;
 	}
 	
+	// Constructor que toma tanto el identificador como el nombre del rol como parámetros.
 	public Rol(Integer roleid, String authority) {
 		this.roleId = roleid;
 		this.authority = authority;
 	}
 	
+	// Método de la interfaz GrantedAuthority que devuelve el nombre del rol (autoridad).
 	@Override
 	public String getAuthority() {	
 		return this.authority;
 	}
-
 }
