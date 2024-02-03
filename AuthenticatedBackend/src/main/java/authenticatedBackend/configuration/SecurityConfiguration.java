3bb8e9d923e8a3e86a2beaea34f3fbfd70a118c5
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-02-02 23:16:32 -0300

    clase de configuración de seguridad explicandola con comentarios

--- AuthenticatedBackend/src/main/java/authenticatedBackend/configuration/SecurityConfiguration.java
+++ AuthenticatedBackend/src/main/java/authenticatedBackend/configuration/SecurityConfiguration.java
@@ -8,15 +8,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfiguration {
 	
+	// Configura la cadena de filtros de seguridad para la aplicación.
 	@SuppressWarnings("deprecation")
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
+                // Deshabilita la protección CSRF (Cross-Site Request Forgery).
                 .csrf(csrf -> csrf.disable())
+                
+                // Configura las reglas de autorización para las solicitudes.
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
+                                // Permite el acceso a cualquier solicitud (puede necesitar ajustes según los requisitos).
                                 .anyRequest().permitAll()
                 )
+                // Construye y devuelve la cadena de filtros de seguridad configurada.
                 .build();				
 	}
 }
